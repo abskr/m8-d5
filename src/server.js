6a12c9dac244dcd9aa90f00361d9e0d1987067d9
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-06-04 13:37:28 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 import express from "express";
-import mongoDB from "./db/index.js";
+import mongoose from "mongoose";
 import accomodationRouter from "./services/accomodation/index.js";
 
 const port = 3001 || process.env.PORT;
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_CONNECTION);
+
 app.use(express.json());
-
 app.use("/accomodation", accomodationRouter);
-app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, mongoDB(), () =>
-  console.log(`Example app listening on port port!`)
-);
+app.listen(port, () => console.log(`listening on port ${port} !`));
